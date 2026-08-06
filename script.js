16669f54c54b3c120b4b2bc82dd27a7b95c0c76f
Karthika T P <karthikapremdas11@gmail.com>
2026-08-07 00:12:14 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,30 @@
+// // 1.display number in text box
+
+function displayNumber(num){
+    result.value +=num
+}
+
+// // 2. clear text value
+
+function clearBox(){
+  result.value= ""
+}
+
+
+// // 3. Evaluate Expression
+
+function evaluateExp(){
+    result.value = eval(result.value)
+}
+
+
+
+// // Remove last iem from text box
+
+function removeLastItem(){
+    result.value = result.value.slice(0,-1)
+}
+
+
+
+///////////////entity codes are in W3 school
